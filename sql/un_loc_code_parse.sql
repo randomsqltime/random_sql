58522a5d9eadba3a036666cf90719552db615709
lewisj <lewisj@users.noreply.github.com>
2017-04-06 15:39:37 +0100

    Update un_loc_code_parse.sql

--- sql/un_loc_code_parse.sql
+++ sql/un_loc_code_parse.sql
@@ -1,16 +1,27 @@
-create table unloc_cords as 
-select *,(case when substring(split_part (coordinates, ' ', 1),5,1) = 'S' then '-'||((substring(split_part (coordinates, ' ', 1),1,2)||'.'||
-substring(split_part (coordinates, ' ', 1),3,2))) else (substring(split_part (coordinates, ' ', 1),1,2)||'.'||
-substring(split_part (coordinates, ' ', 1),3,2)) end) lat, 
-       (case when substring(split_part (coordinates, ' ', 2),6,1) = 'W' then '-'|| ((substring(split_part (coordinates, ' ', 2),1,3)||'.'||
-substring(split_part (coordinates, ' ', 2),4,2))) else (substring(split_part (coordinates, ' ', 2),1,3)||'.'||
-substring(split_part (coordinates, ' ', 2),4,2)) end) lon
+drop table 
+unloc_cords;
+drop table 
+unloc_geo;
 
+
+ create table unloc_cords as
+select *,
+
+substring(split_part (coordinates, ' ', 1),1,2) lat_p1,(substring(split_part (coordinates, ' ', 1),3,2)) lat_p2,
+
+(case when substring(split_part (coordinates, ' ', 1),5,1) = 'S' then -1 else 1 end) lat_multi
+       ,substring(split_part (coordinates, ' ', 2),1,3) lon_p1,
+         substring(split_part (coordinates, ' ', 2),4,2) lon_p2,
+         (case when substring(split_part (coordinates, ' ', 2),6,1) = 'W' then -1 else 1 end) lon_multi
 from unlocode where coordinates is not null;
 
 
-delete from unloc_cords where lon ='.';
+delete from unloc_cords where lat_p2 ='';
+
+select coordinates,((lat_p1::double precision+(lat_p2::double precision/60))*lat_multi),
+((lon_p1::double precision+(lon_p2::double precision/60))*lon_multi) from unloc_cords 
+
 
 create table unloc_geo as
-select *,st_setsrid(st_makepoint(lon::double precision,lat::double precision),4326) from unloc_cords 
-
+select *,st_setsrid(st_makepoint(((lon_p1::double precision+(lon_p2::double precision/60))*lon_multi)
+,((lat_p1::double precision+(lat_p2::double precision/60))*lat_multi)),4326) from unloc_cords 
