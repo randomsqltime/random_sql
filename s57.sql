33188c22802a694c933f080f81e0ba6e18636976
lewisj <lewisj@users.noreply.github.com>
2017-10-31 16:16:11 +0000

    Update s57.sql

--- s57.sql
+++ s57.sql
@@ -1,5 +1,5 @@
 create table bforce.lizard_shore_landing as 
 select * from bforce.lizard_bvow where obj = 'PONTON'  or (
-attr->'CATSLC' = '4' or attr->'CATSLC' = '6' or attr->'CATSLC' = '11'  
+attr->'CATSLC' = '4' or attr->'CATSLC' = '11'  
  or attr->'CATSLC' = '12'  or  attr->'CATSLC' = '13'  or  attr->'CATSLC' = '14')  
 
