2f6fddd17ec2b941fdf3d91859d1be40e3a78134
randomsqltime <33261744+randomsqltime@users.noreply.github.com>
2017-10-31 16:24:50 +0000

    asda
    asd

--- s57.sql
+++ s57.sql
@@ -3,3 +3,4 @@ select * from bforce.lizard_bvow where obj = 'PONTON'  or (
 attr->'CATSLC' = '4' or attr->'CATSLC' = '6' or attr->'CATSLC' = '11'  
  or attr->'CATSLC' = '12'  or  attr->'CATSLC' = '13'  or  attr->'CATSLC' = '14')  
 
+--changes
